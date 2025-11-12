54bd9da730169b39a2b39f45b9d7e2c053d0fd77
jinseoit <jinseoit@gmail.com>
2025-11-13 03:28:11 +0900

    add: 카트 추가

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,20 +2,24 @@ import Component from "@/core/Component";
 import ProductList from "@/components/ProductList";
 import Footer from "@/components/Footer";
 import Header from "../components/Header";
+// import Cart from "../components/Cart";
 
 class HomePage extends Component {
   template() {
     return `
     <header class="header-container"></header>
     <section class="product_list"></section>
+    <section class="cart-container"></section>
     ${Footer()}
     `;
   }
   mounted() {
     const $header = document.querySelector(".header-container");
-    new Header($header);
     const $productList = document.querySelector(".product_list");
+    // const $cart = document.querySelector(".cart-container");
+    new Header($header);
     new ProductList($productList);
+    // new Cart($cart);
   }
 }
 
